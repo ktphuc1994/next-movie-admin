078a26db3d859a5d61a531570f2358a04ac16b19
Thien Phuc Khuc <khucthienphuc@gmail.com>
2023-03-13 00:05:11 +0700

    feat: add movie list table

--- core/components/Movie/List/MovieTable.tsx
+++ core/components/Movie/List/MovieTable.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 
+// import interface and type
 import {
-  alpha,
+  InterfaceMovie,
+  InterfaceMoviePagi,
+} from '../../../interface/models/movie';
+
+// import MUI components
+import {
   Box,
   Checkbox,
-  IconButton,
+  FormControl,
+  FormControlLabel,
+  MenuItem,
+  Paper,
+  Select,
+  SelectChangeEvent,
+  Switch,
+  Table,
+  TableBody,
   TableCell,
+  TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
   TableSortLabel,
   Toolbar,
@@ -14,12 +30,12 @@ import {
   Typography,
 } from '@mui/material';
 import visuallyHidden from '@mui/utils/visuallyHidden';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 // import local library
-import useSWR, { useSWRConfig } from 'swr';
-import { InterfaceMovie } from '../../../interface/models/movie';
+import { useSWRConfig } from 'swr';
+import moment from 'moment';
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -30,25 +46,38 @@ function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   }
   return 0;
 }
+
 type Order = 'asc' | 'desc';
 function getComparator<Key extends keyof any>(
   order: Order,
   orderBy: Key
 ): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
+  a: { [key in Key]: number | string | boolean },
+  b: { [key in Key]: number | string | boolean }
 ) => number {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-interface HeaderId extends InterfaceMovie {
-  action: string;
+function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
 }
+
 interface HeadCell {
   disablePadding: boolean;
-  id: keyof HeaderId;
+  id: keyof InterfaceMovie;
   label: string;
   cellAlign: 'center' | 'left' | 'right' | 'inherit' | 'justify';
 }
@@ -68,60 +97,38 @@ const headCells: readonly HeadCell[] = [
   },
   {
     id: 'dangChieu',
-    cellAlign: 'center',
+    cellAlign: 'right',
     disablePadding: false,
     label: 'Tình trạng',
   },
-  { id: 'hot', cellAlign: 'center', disablePadding: false, label: 'Hot' },
+  { id: 'hot', cellAlign: 'right', disablePadding: false, label: 'Hot' },
   {
     id: 'danhGia',
     cellAlign: 'right',
     disablePadding: false,
     label: 'Đánh giá',
   },
-  { id: 'action', cellAlign: 'center', disablePadding: false, label: 'Action' },
 ];
 
 interface EnhancedTableProps {
   order: Order;
   orderBy: string;
-  numSelected: number;
-  rowCount: number;
   onRequestSort: (
     event: React.MouseEvent<unknown>,
-    property: keyof HeaderId
+    property: keyof InterfaceMovie
   ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-    onSelectAllClick,
-  } = props;
+const EnhancedTableHead = (props: EnhancedTableProps) => {
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler =
-    (property: keyof HeaderId) => (event: React.MouseEvent<unknown>) => {
+    (property: keyof InterfaceMovie) => (event: React.MouseEvent<unknown>) => {
       onRequestSort(event, property);
     };
 
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all movie',
-            }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
@@ -143,73 +150,181 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             </TableSortLabel>
           </TableCell>
         ))}
+        <TableCell key="action" align="center" sortDirection={false}>
+          Action
+        </TableCell>
       </TableRow>
     </TableHead>
   );
-}
+};
 
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-  const { numSelected } = props;
-
+function TableToolbar() {
   return (
     <Toolbar
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Nutrition
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
+      <Typography
+        sx={{ flex: '1 1 100%' }}
+        variant="h6"
+        id="tableTitle"
+        component="div"
+      >
+        Movie List
+      </Typography>
     </Toolbar>
   );
 }
 
 const MovieTable = () => {
   const { cache } = useSWRConfig();
-  const moviePagiCache = cache.get('movieList')?.data;
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<keyof InterfaceMovie>('ngayKhoiChieu');
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  return <div>MovieTable</div>;
+  const moviePagiCache = cache.get('movieList')?.data as
+    | InterfaceMoviePagi
+    | undefined;
+  if (!moviePagiCache) return <div>...Loading</div>;
+  const movieList = moviePagiCache.items;
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof InterfaceMovie
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  const handleSelectStatus = (e: SelectChangeEvent) => {};
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - movieList.length) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableToolbar />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {movieList
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .sort(getComparator(order, orderBy))
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.maPhim}
+                    >
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.maPhim}
+                      </TableCell>
+                      <TableCell align="left">{row.tenPhim}</TableCell>
+                      <TableCell align="right">
+                        {moment(row.ngayKhoiChieu).format('DD/MM/YYYY')}
+                      </TableCell>
+                      <TableCell align="right">
+                        <FormControl size="small">
+                          <Select
+                            value={row.dangChieu.toString()}
+                            onChange={handleSelectStatus}
+                          >
+                            <MenuItem value="true">Đang chiếu</MenuItem>
+                            <MenuItem value="false">Sắp chiếu</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Checkbox color="primary" checked={row.hot} />
+                      </TableCell>
+                      <TableCell align="right">{row.danhGia}</TableCell>
+                      <TableCell align="center">
+                        <Box
+                          component="div"
+                          sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <BorderColorIcon
+                            sx={{ mr: '0.5rem' }}
+                            color="warning"
+                          />
+                          <DeleteOutlineIcon color="error" />
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10]}
+          component="div"
+          count={movieList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
 };
 
 export default MovieTable;
